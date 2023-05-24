4bc7b1008a880f0776160c53f38606294b5a09a1
PhilipeeX <dev.philipe@outlook.com>
2023-05-24 11:08:08 -0300

    método my_all implementado sem erros

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,30 @@
 module Enumerable
   # Your code goes her
+  def my_each_with_index
+    if block_given?
+      i = 0
+      until i == self.length
+        # require 'pry-byebug'; binding.pry
+        yield(self[i], i)
+        i += 1
+      end
+    end
+    self
+  end
+
+  def my_all?
+    if block_given?
+      i = 0
+      until i == self.length
+        if yield(self[i])
+          i += 1
+        else 
+          return false
+        end
+      end
+      return true
+    end
+  end
 
 end
 
